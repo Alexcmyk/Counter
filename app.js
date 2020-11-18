d8d3e3cbe840d3c7dcf6217780a1196d119a26fa
Alexcmyk <alex.kirts@gmail.com>
2020-11-17 20:10:32 -0700

    initail and final commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// Set initial count
+let counter = 0;
+
+// Select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      counter--;
+    } else if (styles.contains("increase")) {
+      counter++;
+    } else {
+      counter = 0;
+    }
+    if (counter > 0) {
+      value.style.color = "green";
+    }
+    if (counter < 0) {
+      value.style.color = "red";
+    }
+    if (counter == 0) {
+      value.style.color = "#222";
+    }
+
+    value.innerHTML = counter;
+  });
+});
